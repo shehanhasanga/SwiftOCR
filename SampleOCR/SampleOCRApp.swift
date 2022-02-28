628288e4afa4e01e1c95af1fb3d589c46ee2fb7d
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-28 21:27:16 +0530

    Initial Commit

---
+++ SampleOCR/SampleOCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleOCRApp.swift
+//  SampleOCR
+//
+//  Created by shehan karunarathna on 2022-02-28.
+//
+
+import SwiftUI
+
+@main
+struct SampleOCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
